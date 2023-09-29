61ab0d8ab684bd64f72d4995a8c2f66ad2ba3e57
Phu <phulk19012002@gmail.com>
2023-09-29 19:14:56 +0700

    First commit

---
+++ src/features/Staff/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+StaffFeature.propTypes = {
+    
+};
+
+function StaffFeature(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default StaffFeature;
