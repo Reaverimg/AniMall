0731aa4e8d5899271cb58f199c9e5e10b740817b
Phu <phulk19012002@gmail.com>
2023-10-02 17:16:11 +0700

    [AM-082] Update

--- src/features/Buyer/index.jsx
+++ src/features/Buyer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
 import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
 import BuyerPage from "./pages/BuyerPage";
-import OrderPage from "./pages/OrderPage";
+
 
 BuyerFeature.propTypes = {};
 
@@ -12,7 +12,7 @@ function BuyerFeature(props) {
     <div>
       <Switch>
         <Route path={match.path} component={BuyerPage} exact></Route>
-        <Route path={`${match.path}/order`} component={OrderPage}></Route>
+        <Route path={`${match.path}/???`} ></Route>
       </Switch>
     </div>
   );
--- src/features/Trainer/index.jsx
+++ src/features/Trainer/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
 import { Switch, Route } from "react-router-dom/cjs/react-router-dom";
 import PropTypes from "prop-types";
+import TrainerCms from "./pages/TrainerCms";
 
 TrainerFeature.propTypes = {};
 
@@ -9,9 +10,8 @@ function TrainerFeature(props) {
   const match = useRouteMatch();
   return (
     <div>
-      Trainer Role
       <Switch>
-        <Route path={match.path} exact></Route>
+        <Route path={match.path} component={TrainerCms} exact></Route>
         <Route path={`${match.path}/:id`}></Route>
       </Switch>
     </div>
