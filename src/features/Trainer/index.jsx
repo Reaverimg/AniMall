eeae630ab62d0bd8cf8cd6177f76fdd698c8fb00
duyle <duylk@tenxtenx.com>
2023-10-14 08:14:57 -0700

    update config

--- src/features/Trainer/index.jsx
+++ src/features/Trainer/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
 import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
-import { Switch, Route } from "react-router-dom/cjs/react-router-dom";
-import PropTypes from "prop-types";
-import TrainerCms from "./pages/TrainerCms";
+import AnimalDetailPage from "./pages/AnimalDetailPage";
+import AnimalManage from "./pages/AnimalManage";
+import FeedingSchedule from "./pages/FeedingSchedule";
+import SpecieDetailPage from "./pages/SpecieDetailPage";
+import SpeciesManage from "./pages/SpeciesManage";
 
 TrainerFeature.propTypes = {};
 
@@ -11,8 +14,28 @@ function TrainerFeature(props) {
   return (
     <div>
       <Switch>
-        <Route path={match.path} component={TrainerCms} exact></Route>
-        <Route path={`${match.path}/:id`}></Route>
+        <Route
+          path={`${match.path}/animalManage`}
+          component={AnimalManage}
+          exact
+        ></Route>
+        <Route
+          path={`${match.path}/animalManage/:idAnimal`}
+          component={AnimalDetailPage}
+        ></Route>
+        <Route
+          path={`${match.path}/speciesManage`}
+          component={SpeciesManage}
+          exact
+        ></Route>
+        <Route
+          path={`${match.path}/speciesManage/:idSpecie`}
+          component={SpecieDetailPage}
+        ></Route>
+        <Route
+          path={`${match.path}/feedingSchedule`}
+          component={FeedingSchedule}
+        ></Route>
       </Switch>
     </div>
   );
