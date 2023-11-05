f49ba27554a8c71543e90f69cc671d812da6bde5
vanhiep <hiepnguyen11702@gmail.com>
2023-11-05 19:14:41 +0700

    Update Landing Page

---
+++ src/components/testing/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "./Navbar";
+// import BannerBackground from "../asset/Untitled (Medium Banner (US) (Landscape)).svg";
+import BannerImage from "../asset/Untitled (Medium Banner (US) (Landscape)) (Logo).svg";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="home-container">
+      <Navbar />
+      <section className="home-banner-container">
+        {/* <section className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </section> */}
+        <section className="home-text-section">
+          <h1 className="primary-heading">
+            Discover the Fascinating World of Wildlife
+          </h1>
+          <p className="primary-text">
+            Our dedicated team of experts takes you on a journey to explore the
+            incredible diversity of animal life in our wildlife sanctuary.
+          </p>
+          <button className="secondary-button">
+            Explore Now <FiArrowRight />{" "}
+          </button>
+        </section>
+        <section className="home-image-container">
+          <img src={BannerImage} alt="" />
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default Home;
--- src/features/Staff/index.jsx
+++ src/features/Staff/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
 import { useRouteMatch } from "react-router-dom/cjs/react-router-dom.min";
 import StaffCms from "./pages/StaffCms";
-import newsManage from "./pages/newsManage";
 
 import TrainerManage from "./pages/TrainerManage";
 import AreaManage from "./pages/areaManage";
 import OrderManage from "./pages/OrderManage/OrderManage";
+import NewsManage from "./pages/newsManage";
+import { CageManage } from "./pages/cageManage";
 
 StaffFeature.propTypes = {};
 // Note
@@ -16,9 +17,10 @@ function StaffFeature(props) {
     <div>
       <Switch>
         <Route path={match.path} component={AreaManage} exact></Route>
-        <Route path={`${match.path}/news`} component={newsManage}></Route>
+        <Route path={`${match.path}/news`} component={NewsManage}></Route>
         <Route path={`${match.path}/trainer-manage`} component={TrainerManage}></Route>
         <Route path={`${match.path}/order-manage`} component={OrderManage}></Route>
+        <Route path={`${match.path}/cages`} component={CageManage}></Route>
       </Switch>
     </div>
   );
